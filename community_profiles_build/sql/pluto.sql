8838fe20f52ce490a06a445778de4b9caa896a7e
Amanda Doyle <AmandaDoyle>
2020-05-21 18:53:36 +0000

    logic to create pluto land use summary tables - need to decide what table name to use and where this should run

---
+++ community_profiles_build/sql/pluto.sql
@@ -0,0 +1,69 @@
+CREATE TABLE cp_landuse_count as (
+SELECT   
+cd,
+COUNT(*) FILTER (WHERE landuse='01') AS "lots_res_1_2_family_bldg",
+COUNT(*) FILTER (WHERE landuse='02') AS "lots_res_multifamily_walkup",
+COUNT(*) FILTER (WHERE landuse='03') AS "lots_res_multifamily_elevator",
+COUNT(*) FILTER (WHERE landuse='04') AS "lots_mixed_use",
+COUNT(*) FILTER (WHERE landuse='05') AS "lots_commercial_office",
+COUNT(*) FILTER (WHERE landuse='06') AS "lots_industrial_manufacturing",
+COUNT(*) FILTER (WHERE landuse='07') AS "lots_transportation_utility",
+COUNT(*) FILTER (WHERE landuse='08') AS "lots_public_facility_institution",
+COUNT(*) FILTER (WHERE landuse='09') AS "lots_open_space",
+COUNT(*) FILTER (WHERE landuse='10') AS "lots_parking",
+COUNT(*) FILTER (WHERE landuse='11') AS "lots_vacant",
+COUNT(*) FILTER (WHERE landuse IS NULL) AS "lots_other_no_data",
+COUNT(*) AS "lots_total"
+FROM dcp_pluto."20v4"
+GROUP BY cd);
+
+CREATE TABLE cp_landuse_area as (
+WITH sumareas as (
+SELECT cd, SUM(lotarea) AS totallotarea
+FROM dcp_pluto."20v4"
+GROUP BY cd),
+landusesums as (
+SELECT   
+cd,
+SUM(lotarea) FILTER (WHERE landuse='01') AS "lot_area___res_1_2_family_bldg",
+SUM(lotarea) FILTER (WHERE landuse='02') AS "lot_area___res_multifamily_walkup",
+SUM(lotarea) FILTER (WHERE landuse='03') AS "lot_area___res_multifamily_elevator",
+SUM(lotarea) FILTER (WHERE landuse='04') AS "lot_area___mixed_use",
+SUM(lotarea) FILTER (WHERE landuse='05') AS "lot_area___commercial_office",
+SUM(lotarea) FILTER (WHERE landuse='06') AS "lot_area___industrial_manufacturing",
+SUM(lotarea) FILTER (WHERE landuse='07') AS "lot_area___transportation_utility",
+SUM(lotarea) FILTER (WHERE landuse='08') AS "lot_area___public_facility_institution",
+SUM(lotarea) FILTER (WHERE landuse='09') AS "lot_area___open_space",
+SUM(lotarea) FILTER (WHERE landuse='10') AS "lot_area___parking",
+SUM(lotarea) FILTER (WHERE landuse='11') AS "lot_area___vacant",
+SUM(lotarea) FILTER (WHERE landuse IS NULL) AS "lot_area___other_no_data",
+SUM(lotarea) AS "total_lot_area"
+FROM dcp_pluto."20v4"  GROUP BY cd)
+SELECT a.cd,
+lot_area___res_1_2_family_bldg,
+round(((lot_area___res_1_2_family_bldg::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___res_1_2_family_bldg,
+lot_area___res_multifamily_walkup,
+round(((lot_area___res_multifamily_walkup::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___res_multifamily_walkup,
+lot_area___res_multifamily_elevator,
+round(((lot_area___res_multifamily_elevator::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___res_multifamily_elevator,
+lot_area___mixed_use,
+round(((lot_area___mixed_use::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___mixed_use,
+lot_area___commercial_office,
+round(((lot_area___commercial_office::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___commercial_office,
+lot_area___industrial_manufacturing,
+round(((lot_area___industrial_manufacturing::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___industrial_manufacturing,
+lot_area___transportation_utility,
+round(((lot_area___transportation_utility::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___transportation_utility,
+lot_area___public_facility_institution,
+round(((lot_area___public_facility_institution::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___public_facility_institution,
+lot_area___open_space,
+round(((lot_area___open_space::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___open_space,
+lot_area___parking,
+round(((lot_area___parking::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___parking,
+lot_area___vacant,
+round(((lot_area___vacant::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___vacant,
+lot_area___other_no_data,
+round(((lot_area___other_no_data::double precision / totallotarea::double precision)*100)::numeric,2) as pct_lot_area___other_no_data,
+total_lot_area
+FROM landusesums a, sumareas b
+WHERE a.cd = b.cd);
