3a09b34508c45a0145e0bf6c946a65e727a0c3ab
sptkl <caobaiyue@gmail.com>
2020-07-06 19:04:44 +0000

    uncomment code

--- cdprofiles_build/01_dataloading.sh
+++ cdprofiles_build/01_dataloading.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 source config.sh
 
-# docker run --rm\
-#     -v $(pwd):/src\
-#     -w /src/python\
-#     -e RECIPE_ENGINE=$RECIPE_ENGINE\
-#     -e BUILD_ENGINE=$BUILD_ENGINE\
-#     nycplanning/cook:latest bash -c "
-#         python3 dataloading.py"
+display "loading CD boundaries"
+docker run --rm\
+    -v $(pwd):/src\
+    -w /src/python\
+    -e RECIPE_ENGINE=$RECIPE_ENGINE\
+    -e BUILD_ENGINE=$BUILD_ENGINE\
+    nycplanning/cook:latest bash -c "
+        python3 dataloading.py"
 
-# docker run --rm\
-#     -v $(pwd):/src\
-#     -w /src/python\
-#     -e API_TOKEN=$API_TOKEN\
-#     -e BUILD_ENGINE=$BUILD_ENGINE\
-#     nycplanning/cook:latest bash -c "
-#         python3 out_crime.py" |  
-#     psql $BUILD_ENGINE -f sql/in_crime.sql
+display "loading crime data"
+docker run --rm\
+    -v $(pwd):/src\
+    -w /src/python\
+    -e API_TOKEN=$API_TOKEN\
+    -e BUILD_ENGINE=$BUILD_ENGINE\
+    nycplanning/cook:latest bash -c "
+        python3 out_crime.py" |  
+    psql $BUILD_ENGINE -f sql/in_crime.sql
 
+display "loading FacDB data"
 psql -q $EDM_DATA -v VERSION=$V_FACDB -f sql/out_facilities.sql | 
     psql $BUILD_ENGINE -f sql/in_facilities.sql &
 
+display "loading PLUTO data"
 psql -q $EDM_DATA -v VERSION=$V_PLUTO -f sql/out_pluto_landusecount.sql | 
-    psql $BUILD_ENGINE -f sql/in_pluto_landusecount.sql
+    psql $BUILD_ENGINE -f sql/in_pluto_landusecount.sql &
 
 psql -q $EDM_DATA -v VERSION=$V_PLUTO -f sql/out_pluto_landusearea.sql |
-    psql $BUILD_ENGINE -f sql/in_pluto_landusearea.sql
+    psql $BUILD_ENGINE -f sql/in_pluto_landusearea.sql &
 
 psql -q $EDM_DATA -v VERSION=$V_PLUTO -f sql/out_floodplain.sql |
-    psql $BUILD_ENGINE -f sql/in_floodplain.sql
+    psql $BUILD_ENGINE -f sql/in_floodplain.sql &
 
+display "loading ACS data"
 psql -q $EDM_DATA -v VERSION=$V_ACS -f sql/out_census.sql |
-    psql $BUILD_ENGINE -f sql/in_census.sql
+    psql $BUILD_ENGINE -f sql/in_census.sql & 
+
+wait
+display "complete"
