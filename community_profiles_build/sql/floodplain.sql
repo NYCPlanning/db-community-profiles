8798d77a1fabf72e064ca9d976107d5c7c214c95
AmandaDoyle <Amanda.Doyle.gis@gmail.com>
2020-07-10 20:17:09 +0000

    adding in community floodplain logic

--- community_profiles_build/sql/floodplain.sql
+++ community_profiles_build/sql/floodplain.sql
@@ -28,8 +28,11 @@ SELECT cd,
   sum(numbldgs) as cd_tot_bldgs,
   sum(CASE WHEN firm07_flag = '1' OR pfirm15_flag = '1' THEN unitsres ELSE 0 END) as fp_100_resunits,
   sum(unitsres) as cd_tot_resunits,
-  sum(CASE WHEN (firm07_flag = '1' OR pfirm15_flag = '1') AND landuse = '09' THEN lotarea ELSE 0 END) as fp_100_openspace
+  sum(CASE WHEN (firm07_flag = '1' OR pfirm15_flag = '1') AND landuse = '09' THEN lotarea ELSE 0 END) as fp_100_openspace,
+  sum(CASE WHEN a.geom&&b.geom AND ST_Intersects(a.geom,b.geom) THEN lotarea ELSE 0 END) as fp_500_area,
+  sum(CASE WHEN a.geom&&b.geom AND ST_Intersects(a.geom,b.geom) AND landuse = '09' THEN lotarea ELSE 0 END) as fp_500_openspace
 INTO FLOODPLAIN_commpro
-FROM dcp_pluto a
+FROM dcp_pluto a,
+(SELECT ST_SubDivide(geom) as geom FROM fema_firms_500yr b) b
 GROUP BY cd
 ORDER BY cd;
