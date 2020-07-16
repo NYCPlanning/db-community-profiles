d85459291ffef5f000dff5940e8319fda51e0e33
sptkl <caobaiyue@gmail.com>
2020-07-16 13:18:23 +0000

    revert rounding

--- cdprofiles_build/sql/out_pluto_landuseareageom.sql
+++ cdprofiles_build/sql/out_pluto_landuseareageom.sql
@@ -1,7 +1,7 @@
 CREATE TEMP TABLE PLUTO_landusearea_commpro as (
     WITH 
     sumareas as (
-        SELECT cd, SUM(ST_Area(geom::geography)) AS totallotarea
+        SELECT cd, SUM(ST_Area(geom::geography::geography)) AS totallotarea
         FROM dcp_pluto.:"VERSION"
         GROUP BY cd
     ),
@@ -26,31 +26,31 @@ CREATE TEMP TABLE PLUTO_landusearea_commpro as (
     )
     SELECT 
         a.cd as borocd,
-        round(lot_area___res_1_2_family_bldg::numeric, 2),
+        lot_area___res_1_2_family_bldg,
         round(((lot_area___res_1_2_family_bldg / totallotarea)::numeric*100),2) as pct_lot_area___res_1_2_family_bldg,
-        round(lot_area___res_multifamily_walkup::numeric, 2),
+        lot_area___res_multifamily_walkup,
         round(((lot_area___res_multifamily_walkup / totallotarea)::numeric*100),2) as pct_lot_area___res_multifamily_walkup,
-        round(lot_area___res_multifamily_elevator::numeric, 2),
+        lot_area___res_multifamily_elevator,
         round(((lot_area___res_multifamily_elevator / totallotarea)::numeric*100),2) as pct_lot_area___res_multifamily_elevator,
-        round(lot_area___mixed_use::numeric, 2),
+        lot_area___mixed_use,
         round(((lot_area___mixed_use / totallotarea)::numeric*100),2) as pct_lot_area___mixed_use,
-        round(lot_area___commercial_office::numeric, 2),
+        lot_area___commercial_office,
         round(((lot_area___commercial_office / totallotarea)::numeric*100),2) as pct_lot_area___commercial_office,
-        round(lot_area___industrial_manufacturing::numeric, 2),
+        lot_area___industrial_manufacturing,
         round(((lot_area___industrial_manufacturing / totallotarea)::numeric*100),2) as pct_lot_area___industrial_manufacturing,
-        round(lot_area___transportation_utility::numeric, 2),
+        lot_area___transportation_utility,
         round(((lot_area___transportation_utility / totallotarea)::numeric*100),2) as pct_lot_area___transportation_utility,
-        round(lot_area___public_facility_institution::numeric, 2),
+        lot_area___public_facility_institution,
         round(((lot_area___public_facility_institution / totallotarea)::numeric*100),2) as pct_lot_area___public_facility_institution,
-        round(lot_area___open_space::numeric, 2),
+        lot_area___open_space,
         round(((lot_area___open_space / totallotarea)::numeric*100),2) as pct_lot_area___open_space,
-        round(lot_area___parking::numeric, 2),
+        lot_area___parking,
         round(((lot_area___parking / totallotarea)::numeric*100),2) as pct_lot_area___parking,
-        round(lot_area___vacant::numeric, 2),
+        lot_area___vacant,
         round(((lot_area___vacant / totallotarea)::numeric*100),2) as pct_lot_area___vacant,
-        round(lot_area___other_no_data::numeric, 2),
+        lot_area___other_no_data,
         round(((lot_area___other_no_data / totallotarea)::numeric*100),2) as pct_lot_area___other_no_data,
-        round(total_lot_area::numeric, 2)
+        total_lot_area
     FROM landusesums a, sumareas b
     WHERE a.cd = b.cd
 );
