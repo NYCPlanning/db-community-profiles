8fb7e07861e8c07a20008ad2292af7113b8b91cf
mgraber <molly.graber@me.com>
2020-09-14 14:21:23 +0000

    Add workflows for "other" variables

--- cdprofiles_build/build.sh
+++ cdprofiles_build/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source config.sh
 
-display "loading CD boundaries"
-docker run --rm\
-    -v $(pwd):/src\
-    -w /src/python\
-    -e RECIPE_ENGINE=$RECIPE_ENGINE\
-    -e BUILD_ENGINE=$BUILD_ENGINE\
-    nycplanning/cook:latest bash -c "
-        python3 dataloading.py"
+# display "loading CD boundaries"
+# docker run --rm\
+#     -v $(pwd):/src\
+#     -w /src/python\
+#     -e RECIPE_ENGINE=$RECIPE_ENGINE\
+#     -e BUILD_ENGINE=$BUILD_ENGINE\
+#     nycplanning/cook:latest bash -c "
+#         python3 dataloading.py"
 
 display "loading crime data"
 docker run --rm\
@@ -22,6 +22,7 @@ docker run --rm\
         python3 out_crime.py" |  
     psql $BUILD_ENGINE -f sql/in_crime.sql
 
+display "loading look-up tables: puma, cd titles, cb contact"
 cat data/cd_puma.csv | psql $BUILD_ENGINE -c "
     DROP TABLE IF EXISTS cd_puma;
     CREATE TABLE cd_puma (
@@ -30,6 +31,30 @@ cat data/cd_puma.csv | psql $BUILD_ENGINE -c "
     ); 
     COPY cd_puma FROM STDIN DELIMITER ',' CSV HEADER;
 "
+
+cat data/cd_titles.csv | psql $BUILD_ENGINE -c "
+    DROP TABLE IF EXISTS cd_titles;
+    CREATE TABLE cd_titles (
+        borocd text,
+        cd_full_title text,
+        cd_short_title text
+    ); 
+    COPY cd_titles FROM STDIN DELIMITER ',' CSV HEADER;
+"
+
+cat data/cb_contact.csv | psql $BUILD_ENGINE -c "
+    DROP TABLE IF EXISTS cb_contact;
+    CREATE TABLE cb_contact (
+        borocd text,
+        cb_email text,
+        cb_website text
+    ); 
+    COPY cb_contact FROM STDIN DELIMITER ',' CSV HEADER;
+"
+
+display "loading CD boundary data"
+psql -q $RECIPE_ENGINE -v VERSION=$V_GEO -f sql/out_cd_geo.sql |
+    psql $BUILD_ENGINE -f sql/in_cd_geo.sql & 
 
 display "loading FacDB data"
 psql -q $EDM_DATA -v VERSION=$V_FACDB -f sql/out_facdb.sql | 
