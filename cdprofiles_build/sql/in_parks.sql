8f002004fd36fb3c0802f13ee2f944badc3f7578
mgraber <molly.graber@me.com>
2020-10-07 14:25:10 +0000

    Set pct_served_parks to NULL for no pop

--- cdprofiles_build/sql/in_parks.sql
+++ cdprofiles_build/sql/in_parks.sql
@@ -12,15 +12,18 @@ WITH geom_tmp AS(
 )
 SELECT
 b.cd as borocd,
-SUM(CASE
-    WHEN b.pop_2010 = 0 THEN 0
-    WHEN b.area = 0 THEN 0
-    WHEN ST_CoveredBy(b.geom, a.geom) 
-        THEN b.pop_2010
-    WHEN NOT ST_Intersects(a.geom, b.geom) THEN 0
-    ELSE 
-        ST_AREA(ST_INTERSECTION(a.geom, b.geom))* b.pop_2010 / b.area
-END) / SUM(b.pop_2010) as pct_served_parks
+(CASE WHEN SUM(b.pop_2010) = 0 THEN NULL
+    ELSE
+    SUM(CASE
+        WHEN b.pop_2010 = 0 THEN 0
+        WHEN b.area = 0 THEN 0
+        WHEN ST_CoveredBy(b.geom, a.geom) 
+            THEN b.pop_2010
+        WHEN NOT ST_Intersects(a.geom, b.geom) THEN 0
+        ELSE 
+            ST_AREA(ST_INTERSECTION(a.geom, b.geom))* b.pop_2010 / b.area
+    END) / SUM(b.pop_2010)
+END) as pct_served_parks
 INTO PARKS
 FROM geom_tmp a, bctcb_pop_2010 b
 GROUP BY borocd;
