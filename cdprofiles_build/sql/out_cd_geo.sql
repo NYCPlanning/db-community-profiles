52cc72a4860f5dc6037a326acfc0aa7b1193630b
mgraber <molly.graber@me.com>
2020-11-27 20:13:15 +0000

    Pull latest geo & facdb, set env first

--- cdprofiles_build/sql/out_cd_geo.sql
+++ cdprofiles_build/sql/out_cd_geo.sql
@@ -4,7 +4,7 @@ CREATE TEMP TABLE cd_geo as (
         ST_Area(wkb_geometry::geography) / 4046.8564224 as acres,
         ST_Area(wkb_geometry::geography) / 1609.34^2 as area_sqmi,
         wkb_geometry
-    FROM dcp_cdboundaries.:"VERSION"
+    FROM dcp_cdboundaries.latest
 );
 
 \COPY cd_geo TO PSTDOUT DELIMITER ',' CSV HEADER;
