5c9d9947606a16198809672dc07ea3974fd360d7
sptkl <caobaiyue@gmail.com>
2021-02-02 18:57:27 +0000

    adding decennial source

--- cdprofiles_build/python/out_parks.py
+++ cdprofiles_build/python/out_parks.py
@@ -3,7 +3,11 @@ import os
 import sys
 from factfinder.main import Pff
 
-decennial = Pff(api_key=os.environ['CENSUS_API_KEY'], year=int(os.environ['V_DECENNIAL']))
+decennial = Pff(
+    api_key=os.environ['CENSUS_API_KEY'], 
+    year=int(os.environ['V_DECENNIAL']), 
+    source='decennial'
+)
 
 df_with_access = decennial.calculate('decennial_pop', 'cd_park_access')
 df_total = decennial.calculate('decennial_pop', 'cd')
