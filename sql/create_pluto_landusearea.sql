42cedaa8f189ec995f8c817b44a5183fa56b23d1
sptkl <caobaiyue@gmail.com>
2021-10-14 18:43:29 +0000

    use lot centroid to do spatial join with cdta spatial boundary

--- sql/create_pluto_landusearea.sql
+++ sql/create_pluto_landusearea.sql
@@ -1,32 +1,7 @@
 DROP TABLE IF EXISTS PLUTO_landusearea_commpro;
 CREATE TABLE PLUTO_landusearea_commpro as (
-    WITH 
-    sumareas as (
-        SELECT cd, SUM(ST_Area(wkb_geometry::geography::geography)) AS totallotarea
-        FROM dcp_mappluto
-        GROUP BY cd
-    ),
-    landusesums as (
-        SELECT
-            cd,
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='01') AS "lot_area___res_1_2_family_bldg",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='02') AS "lot_area___res_multifamily_walkup",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='03') AS "lot_area___res_multifamily_elevator",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='04') AS "lot_area___mixed_use",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='05') AS "lot_area___commercial_office",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='06') AS "lot_area___industrial_manufacturing",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='07') AS "lot_area___transportation_utility",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='08') AS "lot_area___public_facility_institution",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='09') AS "lot_area___open_space",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='10') AS "lot_area___parking",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse='11') AS "lot_area___vacant",
-            SUM(ST_Area(wkb_geometry::geography)) FILTER (WHERE landuse IS NULL) AS "lot_area___other_no_data",
-            SUM(ST_Area(wkb_geometry::geography)) AS "total_lot_area"
-        FROM dcp_mappluto  
-        GROUP BY cd
-    )
-    SELECT 
-        a.cd as borocd,
+    SELECT
+        cdta2020,
         lot_area___res_1_2_family_bldg::integer,
         round(((lot_area___res_1_2_family_bldg / totallotarea)::numeric*100),2) as pct_lot_area___res_1_2_family_bldg,
         lot_area___res_multifamily_walkup::integer,
@@ -52,6 +27,25 @@ CREATE TABLE PLUTO_landusearea_commpro as (
         lot_area___other_no_data::integer,
         round(((lot_area___other_no_data / totallotarea)::numeric*100),2) as pct_lot_area___other_no_data,
         total_lot_area::integer
-    FROM landusesums a, sumareas b
-    WHERE a.cd = b.cd
+    FROM (
+        SELECT
+            dcp_cdta2020.cdta2020,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) AS totallotarea,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='01') AS lot_area___res_1_2_family_bldg,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='02') AS lot_area___res_multifamily_walkup,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='03') AS lot_area___res_multifamily_elevator,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='04') AS lot_area___mixed_use,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='05') AS lot_area___commercial_office,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='06') AS lot_area___industrial_manufacturing,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='07') AS lot_area___transportation_utility,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='08') AS lot_area___public_facility_institution,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='09') AS lot_area___open_space,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='10') AS lot_area___parking,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse='11') AS lot_area___vacant,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) FILTER (WHERE landuse IS NULL) AS lot_area___other_no_data,
+            SUM(ST_Area(dcp_mappluto.wkb_geometry::geography)) AS total_lot_area 
+        FROM dcp_mappluto LEFT JOIN dcp_cdta2020 
+        ON ST_Within(ST_SetSRID(ST_MakePoint(longitude, latitude), 4326), dcp_cdta2020.wkb_geometry)
+        GROUP BY dcp_cdta2020.cdta2020
+    ) a
 );
